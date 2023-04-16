0b22f04668966941e7cecf39d053734096d310fa
Fabian Abad <89323601+SedronDev@users.noreply.github.com>
2023-04-16 03:34:15 -0500

    Delete build directory

--- build/_app/immutable/chunks/2.a1b20437.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.74ed6891.js";export{t as component};
--- build/_app/immutable/chunks/3.6e9ba2ab.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/game-page.svelte.bff914bd.js";export{t as component};
