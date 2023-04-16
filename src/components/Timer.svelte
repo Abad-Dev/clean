79c4106197332c746d2d05fe1a02967a22d7e265
sedron <jfabianabadd@gmail.com>
2023-04-15 22:57:08 -0500

    Initial commit and testin

---
+++ src/components/Timer.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let limitTime = 1;
+    export let initialTime = 0;
+
+    export let time = initialTime;
+    
+    const interval = setInterval(() => {time++}, 1000)
+
+    $: {
+        if (time >= limitTime ){
+            clearInterval(interval)
+        }
+    }
+</script>
+
+<h1 class="text-center">{time}</h1>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Timer from "../components/Timer.svelte";
+
+    let currentTime = 0;
+
+    
+</script>
+
+<div class="container-fluid d-flex align-items-center justify-content-center" style="min-height: 100vh; background-color: #323232">
+    <div class="row text-light">
+        <div class="col-12">
+            <h1 class="text-center">
+                <Timer bind:time={currentTime} />
+            </h1>
+        </div>
+    </div>
+</div>
+
